96290b9548f7c6fdc330673fa0b0bd62629a6ec7
sultan <sultan2eu1@gmail.com>
2020-12-28 22:04:46 +0300

    add visibility

--- src/utils/forcast.js
+++ src/utils/forcast.js
@@ -1,5 +1,7 @@
 const request = require('request');
 
+
+
 const forcast = (latitude,longitude,callback) => {
     const url = `http://api.weatherstack.com/current?access_key=0f14661e4a320809b707e8e3e86ab2e1&query=${latitude},${longitude}`
 
@@ -9,7 +11,7 @@ const forcast = (latitude,longitude,callback) => {
         } else if (body.error){
             callback('Please specify a valid location identifier',undefined)
         } else{
-            callback(undefined, ` ${body.current.weather_descriptions[0]} It's is current ${body.current.temperature} out. it feels like ${body.current.feelslike}`)
+            callback(undefined, ` ${body.current.weather_descriptions[0]} It's is current ${body.current.temperature} out. it feels like ${body.current.feelslike}, the visibility ${body.current.visibility}`)
         }
     })
 }
